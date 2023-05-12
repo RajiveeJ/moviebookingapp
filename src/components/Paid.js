4e2410806f4e20e6daf2cf39d78df29b4d48396a
RajiveeJ <99588388+RajiveeJ@users.noreply.github.com>
2023-05-12 12:13:55 +0530

    Create Paid.js

---
+++ src/components/Paid.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Paid() {
+    
+    return (
+        <div>
+            Successfully booked tickets and sent to mail, Enjoy your movie time!!!!!!!!!!!!!
+        </div>
+     
+    );
+}
